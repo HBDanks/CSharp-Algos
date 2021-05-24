ec2a4308984cd2117786649d2c9360f5a5ec873a
Harold Danks <44505134+HBDanks@users.noreply.github.com>
2021-05-24 10:14:22 -0400

    Update MultArr.js
    - Add Comments explaining the purpose of the problem and the function

--- MultArr.js
+++ MultArr.js
@@ -1,1 +1,16 @@
-const MultipleArrays = (num, len) => Array.from({length: len}, (x, i) => i*num);
+/**
+ * arrayOfMultiples
+ * 
+ * Creates an array where each index is equal to the value of the index multiplied
+ * by the provided number.
+ * 
+ * @param {int} num - The number by which to multiply each index
+ * @param {int} len - The length of the desired array
+ * 
+ * @returns Array
+ */
+const arrayOfMultiples = (num, len) => Array.from({length: len}, (x, i) => i*num);
+
+// While this problem is not solved in C#, it was a question asked on LinkedIn
+// that I knew how to solve using System.Linq and wanted to solve in a similar way
+// Using JavaScript, this is the solution I came up with
